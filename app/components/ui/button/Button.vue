82b4310e67d446cc4548c071fb486f87c884837a
Heang Virith <virith.id@gmail.com>
2026-01-21 09:57:33 +0700

    feat: Integrate Shadcn Vue components and configure Tailwind CSS with a utility function.

---
+++ app/components/ui/button/Button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+	import type { HTMLAttributes } from "vue"
+	import type { ButtonVariants } from "."
+	import { Primitive } from "reka-ui"
+	import { cn } from "@/lib/utils"
+	import { buttonVariants } from "."
+
+	const props = withDefaults(defineProps<{
+		variant?: ButtonVariants["variant"]
+		size?: ButtonVariants["size"]
+		class?: HTMLAttributes["class"]
+		as?: string
+		asChild?: boolean
+	}>(), {
+		as: "button",
+	})
+</script>
+
+<template>
+	<Primitive data-slot="button" :as="as" :as-child="asChild"
+		:class="cn(buttonVariants({ variant, size }), props.class)">
+		<slot />
+	</Primitive>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	modules: ['shadcn-nuxt'],
+
+	css: ['~/assets/css/tailwind.css'],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	shadcn: {
+		/**
+		 * Prefix for all the imported component.
+		 * @default "Ui"
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "@/components/ui"
+		 */
+		componentDir: '@/components/ui',
+	},
+});
